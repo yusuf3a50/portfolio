c716cd5b562b1412139974b94cb0ed38dcff52c6
yusuf3a50 <ybyphone@disroot.org>
2023-07-31 22:47:13 +0100

    update contacts page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../css/main.css';
+import email from '../imgs/email.png'
+import { BsLinkedin } from 'react-icons/bs';
 
 function Contact() {
     return (
         <>
-        <h1>Contact Page</h1>
-        <p><b>LinkedIn </b>
-            <a href="https://linkedin.com/in/yusuf3a50/" target="_blank" rel="noopener noreferrer">
-            My linkedIn profile can be found here</a></p>
-            <p><b>Email </b></p>
+            <h1>Contact Page</h1>
+            <p>
+                <a href="https://linkedin.com/in/yusuf3a50/" target="_blank" rel="noopener noreferrer">
+                    
+                    My linked<BsLinkedin /> profile can be found here</a></p>
+            <p>Email:
+                <img src={email} alt="image of email address text" />
+            </p>
         </>
     );
 }
